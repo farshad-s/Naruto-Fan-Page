483016a3b0bbafcde754585f33ae0f7996d6bd7f
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-13 20:17:32 +0000

    added question text + array for quiz questions + display the question when start button is clicked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 const startButton = document.getElementById("start-button");
 const showQuestions = document.getElementById("quiz-container");
 const nextButton = document.getElementById("next-button");
+const questionText = document.getElementById("question-text");
 
 startButton.addEventListener("click", startQuiz);
 
@@ -8,4 +9,17 @@ function startQuiz() {
   startButton.classList.add("hide");
   showQuestions.classList.remove("hide");
   nextButton.classList.remove("hide");
+  questionText.innerText = quizArray[0].question;
 }
+
+const quizArray = [
+  {
+    question: "What is the name of the main character?",
+    answer1: "Sasuke Uchiha",
+    answer2: "The 3rd Hokage",
+    answer3: "Naruto Uzumaki",
+    answer4: "Kakashi Sensei",
+  },
+];
+
+console.log(questionArray[0].question);
