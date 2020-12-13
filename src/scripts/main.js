73d0a47e6b4813fa1aabd88ce654e62b6517b5ce
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-13 20:10:31 +0000

    click on start button, shows questions + next button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,11 @@
 const startButton = document.getElementById("start-button");
+const showQuestions = document.getElementById("quiz-container");
+const nextButton = document.getElementById("next-button");
 
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
   startButton.classList.add("hide");
+  showQuestions.classList.remove("hide");
+  nextButton.classList.remove("hide");
 }
