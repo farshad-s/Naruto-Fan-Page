2d8cfa1f3d9aea5456d30df9be6c3a088d6e60a7
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-13 20:29:54 +0000

    added the four buttons, tested the buttons with the start quiz function, created function for picking random question, created more questions and answers in the array

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,12 @@ const showQuestions = document.getElementById("quiz-container");
 const nextButton = document.getElementById("next-button");
 const questionText = document.getElementById("question-text");
 
-startButton.addEventListener("click", startQuiz);
+const answerOne = document.getElementById("answer-one");
+const answerTwo = document.getElementById("answer-two");
+const answerThree = document.getElementById("answer-three");
+const answerFour = document.getElementById("answer-four");
 
-function startQuiz() {
-  startButton.classList.add("hide");
-  showQuestions.classList.remove("hide");
-  nextButton.classList.remove("hide");
-  questionText.innerText = quizArray[0].question;
-}
+startButton.addEventListener("click", startQuiz);
 
 const quizArray = [
   {
@@ -20,6 +18,42 @@ const quizArray = [
     answer3: "Naruto Uzumaki",
     answer4: "Kakashi Sensei",
   },
+  {
+    question: "What is Naruto Uzumakis favourite food?",
+    answer1: "Ramen",
+    answer2: "Steak",
+    answer3: "He doesn't like food",
+    answer4: "Apples",
+  },
+  {
+    question: "What power do the Uchiha clan possess",
+    answer1: "The Renegan",
+    answer2: "The Sharingan",
+    answer3: "The Byakugan",
+    answer4: "The Nine Tails Chakra",
+  },
+  {
+    question: "Which tailed beast does the Raikages brother control?",
+    answer1: "The three tails",
+    answer2: "The nine tails",
+    answer3: "The eight tails",
+    answer4: "The five tails",
+  },
 ];
 
-console.log(questionArray[0].question);
+function startQuiz() {
+  startButton.classList.add("hide");
+  showQuestions.classList.remove("hide");
+  nextButton.classList.remove("hide");
+  questionText.innerText = quizArray[0].question;
+  answerOne.innerText = quizArray[0].answer1;
+  answerTwo.innerText = quizArray[0].answer2;
+  answerThree.innerText = quizArray[0].answer3;
+  answerFour.innerText = quizArray[0].answer4;
+}
+
+function pickRandomQuestion() {
+  Math.floor(Math.random() * quizArray.length);
+}
+
+pickRandomQuestion();
